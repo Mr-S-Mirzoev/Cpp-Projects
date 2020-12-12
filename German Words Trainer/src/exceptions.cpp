066f442db69f7297cddabfb17a81b00729ae2169
Sergey Mirzoev <61053551+Mr-S-Mirzoev@users.noreply.github.com>
2020-12-12 12:32:06 +0300

    Update exceptions.cpp

--- German Words Trainer/src/exceptions.cpp
+++ German Words Trainer/src/exceptions.cpp
@@ -1,4 +1,7 @@
 #include "exceptions.hpp"
 
+DeutschBaseException::DeutschBaseException(const std::string &what):
+    std::exception(what) {}
+
 DeutschTypeAssertException::DeutschTypeAssertException(const std::string &type, std::string where):
     DeutschBaseException("[DeutschTypeAssertException] Type assertion failed for type " + type + ((!where.empty()) ? "in " + where : "")) {}
