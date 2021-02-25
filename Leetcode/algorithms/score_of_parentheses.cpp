3c88f2281e6ea96b47cc19c92bf37167ce1d98e4
Mr-S-Mirzoev <mrsergeymirzoev@gmail.com>
2021-02-25 12:36:18 +0300

    Space Efficient solution

--- Leetcode/algorithms/score_of_parentheses.cpp
+++ Leetcode/algorithms/score_of_parentheses.cpp
@@ -1,73 +1,50 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
 
-class TreeNode {
-    TreeNode *parent_;
-    std::vector <TreeNode *> childs_;
-public:
-    TreeNode(TreeNode *parent = nullptr): parent_(parent) {}
-    TreeNode* append() {
-        auto ptr = new TreeNode(this);
-        childs_.push_back(ptr);
-        return ptr;
-    }
-
-    TreeNode* get_parent() const {
-        return parent_;
-    }
-
-    void print(int tb = 0) const {
-        std::string s(tb, '\t');
-        std::cout << s << "(sz=" << childs_.size() << std::endl;
-        for (auto child : childs_) {
-            child->print(tb + 1);
-        }
-    }
-
-    int count() const {
-        if (childs_.size() == 0) {
-            return 1;
-        } else {
-            int sum = 0;
-            for (auto child : childs_) {
-                sum += child->count();
-            }
-            return sum * 2;
-        }
-    }
-
-    int root_count() const {
-        int sum = 0;
-        for (auto child : childs_) {
-            sum += child->count();
-        }
-        return sum;
-    }
-};
-
-void tn_from_string(const std::string &s, int &i, TreeNode *tn) {
+int count_all(const std::string &s, int &i) {
+    int sum = 0;
+    int count = 0;
     while (i < s.length()) {
         if (s[i] == '(') {
-            auto new_tn = tn->append();
+            ++count;
             ++i;
-            tn_from_string(s, i, new_tn);
+            sum += count_all(s, i);
         } else {
             ++i;
-            return;
+            if (count == 0) {
+                return 1;
+            } else {
+                return sum * 2;
+            }
         }
     }
+    if (count == 0) {
+        return 1;
+    } else {
+        return sum * 2;
+    }
+}
+
+int root_count_all(const std::string &s, int &i) {
+    int sum = 0;
+    while (i < s.length()) {
+        if (s[i] == '(') {
+            ++i;
+            sum += count_all(s, i);
+        } else {
+            ++i;
+            return sum;
+        }
+    }
+    return sum;
 }
 
 class Solution {
 public:
     int scoreOfParentheses(std::string input) {
         int i = 0;
-        auto root = new TreeNode();
-        tn_from_string(input, i, root);
-        //root->print();
-        return root->root_count();
+        int many = root_count_all(input, i);
+        return many;
     }
 };
 
