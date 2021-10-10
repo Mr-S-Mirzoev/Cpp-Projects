73ab322cf913303a4f70b755f3cb7cf0f9faef2d
Mr-S-Mirzoev <mrsergeymirzoev@gmail.com>
2021-10-10 14:55:45 +0300

    Get rid of C++ optimization

--- Studies/Optimization/src/newton.cc
+++
@@ -1,9 +0,0 @@
-#include "newton.hpp"
-
-linear_algebra::Matrix NewtonOptimizer::minimize(std::function<double (const linear_algebra::Matrix &)> f, const linear_algebra::Matrix & start) {
-    /* x_new = x − f''(x)^−1 * f'(x) */
-
-    {\displaystyle ,\qquad k\geq 0.}
-
-    linear_algebra::Matrix x_new = start - (Jakobian(f, x)^(-1)) * 
-}
