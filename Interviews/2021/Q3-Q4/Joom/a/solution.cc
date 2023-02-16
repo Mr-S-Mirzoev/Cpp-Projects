8e7b8c7f6e79a4c43ffcb81563ea6802f754760a
Sergey Mirzoev <61053551+Mr-S-Mirzoev@users.noreply.github.com>
2023-02-16 23:55:35 +0100

    Unfinished code (#8)
    * Enhanced readability of code
    
    * Fixed logic error in foo-bar code
    
    * Added new tasks
    
    * Folder structure and CMake Beautification
    
    * Updated Readme
    
    * Enhanced folder structure and beautified CMakes
    
    * Updated structure of executables in bin
    
    * Added solutions for two tasks in Leetcode
    
    * First Bad Version solution using bin-search
    
    * Easy task solution from LeetCode
    
    * Add SG code
    
    * Removed unsuccessfull project
    
    * Removed submodule
    
    * Create LICENSE (#6)
    
    * Create CODE_OF_CONDUCT.md
    
    * Optimization task for practicum
    
    * Add inversion, power to linalg library
    
    * Update studying project
    
    * Update lib README
    
    * Get rid of C++ optimization
    
    * Fix structure, remove old
    
    * Rename DP: Command
    
    * Restructured interviews
    
    * Refactor structure and update README.md
    
    * VK.Education submodule
    
    * Fix readme link to VK.Edu
    
    * Fix headers
    
    * Fix AVSW link
    
    * Projects from ASD 2021
    
    * Add leetcode updates from Oct'21
    
    * Add JOOM Interview tasks and solutions
    
    * Fix indents
    
    * Fix template
    
    * Add task from WunderFund interview
    
    * Add task and solution.cc
    
    * Fix the code
    
    * Add CLI
    
    * Add trailing newline to task description
    
    * Fix usual binary search to binary search leftmost
    
    * Repo restructure
    
    * Add latest interview tasks and solutions
    
    * Add hard Leetcode task
    
    * fix latex in desc
    
    * fix latex in desc
    
    * Add Leetcode build
    
    * Substring with Concatenation of All Words
    
    * Add fucked-up leetcode task
    
    * Tidy up code (dedup, rename)
    
    ---------
    
    Co-authored-by: Sergey Mirzoev <sergey.mirzoev@kaspersky.com>

---
+++ Interviews/2021/Q3-Q4/Joom/a/solution.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+std::vector <long long unsigned> squares(std::vector <long> const& input) {
+    std::size_t N = input.size();
+
+    if (N == 0) 
+        return {};
+
+    std::size_t l = 0, r = N - 1, i = N - 1;
+    long long left_val, right_val;
+
+    std::vector <long long unsigned> out(N);
+
+    while (i < N) {
+        left_val = input[l];
+        right_val = input[r];
+
+        if (left_val >= 0) {
+            out[i--] = right_val * right_val;
+            --r;
+        } else if (right_val < 0) {
+            out[i--] = left_val * left_val;
+            ++l;
+        } else {
+            if (-left_val > right_val) {
+                out[i--] = left_val * left_val;
+                ++l;
+            } else {
+                out[i--] = right_val * right_val;
+                --r;
+            }
+        }
+    }
+
+    return out;
+}
+
+int main(int argc, char const *argv[])
+{
+    for (auto v : squares({-5, -2, -1, 0, 4, 7}))
+        std::cout << v << " ";
+    std::cout << std::endl;
+
+    for (auto v : squares({-5, -2, -1, 4, 7}))
+        std::cout << v << " ";
+    std::cout << std::endl;
+
+    for (auto v : squares({0, 4, 7}))
+        std::cout << v << " ";
+    std::cout << std::endl;
+
+    for (auto v : squares({-5, -2, -1}))
+        std::cout << v << " ";
+    std::cout << std::endl;
+    
+    return 0;
+}
---
+++ Interviews/2021/Q3-Q4/Wunderfund/solution.cc
@@ -0,0 +1,64 @@
+#include <set>
+#include <queue>
+
+class MinFriendlyCRUD {
+    std::set <int> set_;
+    std::queue <std::set<int>::iterator> queue_;
+    std::size_t N_;
+public:
+    MinFriendlyCRUD (std::size_t N):
+        N_(N) {}
+
+    void update(int price) {
+        if (queue_.size() == N_) {
+            set_.erase(queue_.front());
+            queue_.pop();
+        }
+
+        auto inserion = set_.insert(price);
+        if (inserion.second)
+            queue_.push(inserion.first);
+        else
+            throw std::logic_error("Failed to insert node to tree, possible memory error");
+    }
+
+    int getMinPriceInWindow() const {
+        return *set_.cbegin();
+    }
+};
+
+enum OpType {
+    INSERT,
+    GET,
+    EXIT
+};
+
+int main(int argc, char const *argv[])
+{
+    std::size_t N;
+    scanf("%lu", &N);
+    MinFriendlyCRUD storage(N);
+
+    int op;
+
+    while (1) {
+        scanf("%d", &op);
+
+        switch (op)
+        {
+        case INSERT:
+            int val;
+            scanf("%d", &val);
+            storage.update(val);
+            break;
+
+        case GET:
+            printf("%d\n", storage.getMinPriceInWindow());
+            break;
+        
+        default:
+            return 0;
+        }
+    }
+    return 0;
+}
